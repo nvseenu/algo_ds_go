a1bd7cc58946bebe4c7eb62d2897a5e8b8a83b86
Srinivasan Natarajan <snatarajan@ceb.com>
2017-07-06 14:01:23 +0530

    Implemented bubble sort

---
+++ src/algo_ds/sort/bubble_sort.go
@@ -0,0 +1,22 @@
+package sort
+
+import "algo_ds/util"
+
+type BubbleSort struct {
+	compare func(a interface{}, b interface{}) int
+}
+
+func (b BubbleSort) Sort(arr []interface{}) {
+
+	for i := len(arr) - 1; i > 0; i-- {
+		for j := 0; j < i; j++ {
+			if b.compare(arr[j], arr[j+1]) < 0 {
+				util.Swap(arr, j, j+1)
+			}
+		}
+	}
+}
+
+func NewBubbleSort(compare func(a interface{}, b interface{}) int) *BubbleSort {
+	return &BubbleSort{compare}
+}
